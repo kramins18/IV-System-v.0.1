50c471ed746739a8a36a2161b740b0445bd36de1
Arvīds Kramiņš <arvids.kramins@bbit.tech>
2020-09-11 11:19:09 +0300

    Added admin class

---
+++ InternetVeikals/Models/Users/Admin.cs
@@ -0,0 +1,26 @@
+﻿namespace InternetVeikals.Models.Users
+{
+    /// <summary>
+    /// Administrators class
+    /// </summary>
+    /// <seealso cref="InternetVeikals.Models.Users.User" />
+    public class Admin : User
+    {
+        /// <summary>
+        /// Gets or sets the salary.
+        /// </summary>
+        /// <value>
+        /// Value of salary.
+        /// </value>
+        public double Salary { get; set; }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Admin"/> class.
+        /// And sets role to admin
+        /// </summary>
+        public Admin()
+        {
+            role = Role.Admin;
+        }
+
+    }
+}
